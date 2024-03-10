d5eeb0b584b3277e468b1a6a994593bd9d3e85e9
stmuel <39916027+st-muel@users.noreply.github.com>
2024-03-10 12:32:43 +0800

    feat: added frontend page

---
+++ src/app/components/Spinner.tsx
@@ -0,0 +1,10 @@
+
+const Spinner = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <div className="animate-spin rounded-full h-4 w-4 border-b border-gray-700" />
+        </div>
+    );
+}
+
+export default Spinner;
